f6bd8cfdbb1c088ce2f3c572f8b1b8169a09cf2a
varunjoshi799 <varunjoshi799@gmail.com>
2023-08-13 14:04:14 -0700

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const app = express();
-
-// import the routes file
-const routes = require("./routes/routes")
+const cloudinary = require("cloudinary").v2;
+const bodyParser = require('body-parser');
+require('dotenv').config()
 
 // body parser configuration
-const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-// register the routes 
-app.use('/', routes);
+// cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+// image upload API
+app.post("/image-upload", (request, response) => {
+    // collected image from a user
+    const data = {
+      image: request.body.image,
+    }
+
+    // upload image here
+    cloudinary.uploader.upload(data.image)
+    .then((result) => {
+      response.status(200).send({
+        message: "success",
+        result,
+      });
+    }).catch((error) => {
+      response.status(500).send({
+        message: "failure",
+        error,
+      });
+    });
+});
 
 module.exports = app;
