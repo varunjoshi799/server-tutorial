975f796f2a0771a5329aac7558a04b9a27d174ae
varunjoshi799 <varunjoshi799@gmail.com>
2023-08-14 07:05:46 -0700

    created persist image endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cloudinary = require("cloudinary").v2;
 const bodyParser = require("body-parser");
+const db = require('./services/dbConnect.js')
 require("dotenv").config();
 
 app.use(bodyParser.json());
@@ -43,5 +44,52 @@ app.post("/image-upload", (req, res) => {
             });
         });
 });
+
+// persist image
+app.post("/persist-image", (request, response) => {
+    // collected image from a user
+    const data = {
+      title: request.body.title,
+      image: request.body.image
+    }
+  
+    // upload image here
+    cloudinary.uploader.upload(data.image)
+    .then((image) => {
+      db.pool.connect((err, client) => {
+        // inset query to run if the upload to cloudinary is successful
+        const insertQuery =` INSERT INTO images (title, cloudinary_id, image_url) 
+           VALUES($1,$2,$3) RETURNING *`;
+        const values = [data.title, image.public_id, image.secure_url];
+  
+        // execute query
+        client.query(insertQuery, values)
+        .then((result) => {
+          result = result.rows[0];
+  
+          // send success response
+          response.status(201).send({
+            status: "success",
+            data: {
+              message: "Image Uploaded Successfully",
+              title: result.title,
+              cloudinary_id: result.cloudinary_id,
+              image_url: result.image_url,
+            },
+          })
+        }).catch((e) => {
+          response.status(500).send({
+            message: "failure",
+            e,
+          });
+        })
+      })  
+    }).catch((error) => {
+      response.status(500).send({
+        message: "failure",
+        error,
+      });
+    });
+  });
 
 module.exports = app;
